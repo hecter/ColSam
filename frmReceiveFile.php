970fea84a811142a19934769bebcd3c1c8c0fccc
Hec Ter <tuhec@hotmail.com>
2014-06-01 14:00:21 -0500

    Inicio

---
+++ frmReceiveFile.php
@@ -0,0 +1,22 @@
+
+<?php
+// En versiones de PHP anteriores a 4.1.0, $HTTP_POST_FILES debe utilizarse en lugar
+// de $_FILES.
+
+$uploaddir = '';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+if ( move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "El archivo es válido y fue cargado exitosamente.\n";
+} else {
+    echo "¡Posible ataque de carga de archivos!\n";
+}
+
+echo 'Aquí hay más información de depurado:';
+print_r($_FILES);
+
+print "</pre>";
+
+?>
